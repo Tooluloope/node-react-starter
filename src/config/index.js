e27d95034ef6c4d41fbf1d2769b40a050d47a9a0
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2020-05-04 03:40:11 +0100

    configuring env variables

---
+++ src/config/index.js
@@ -0,0 +1,31 @@
+import { merge } from 'lodash'
+const env = process.env.NODE_ENV || 'development'
+
+const baseConfig = {
+  env,
+  isDev: env === 'development',
+  isTest: env === 'testing',
+  isTest: env === 'production',
+  port: 5000,
+}
+
+let envConfig = {}
+
+switch (env) {
+  case 'dev':
+  case 'development':
+    envConfig = require('./dev').config
+    break
+  case 'test':
+  case 'testing':
+    envConfig = require('./testing').config
+    break
+  case 'prod':
+    case 'production':
+      envConfig = require('./prod').config
+      break
+  default:
+    envConfig = require('./dev').config
+}
+
+export default merge(baseConfig, envConfig)
