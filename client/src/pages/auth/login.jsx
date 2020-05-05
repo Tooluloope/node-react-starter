da6e486ca9fc2bc4cc792c70568bce7bb47a9174
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2020-05-05 21:43:38 +0100

    Auth refactor

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import phone  from "../../assets/undraw_personalization_triu.svg";
 import wave  from "../../assets/wave.png";
 import profile  from "../../assets/undraw_profile_pic_ic5t.svg";
-import "./s.css";
+import "./auth.css";
 import { Input, ButtonAuth } from "../../components/inputs/input";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -11,6 +11,7 @@ import axios from "axios";
 
 
 export const Login = () => {
+    // Initial State of the app during login
     const initialState = {
         user: {
             password:"",
@@ -22,44 +23,52 @@ export const Login = () => {
     const [state, setState] = useState(initialState);
     
 
-
+// handles Event changes in the fields
     const handleChange = event => {
         const { user } = state;
         user[event.target.name] = event.target.value;
         setState({...state, user});
     };
+
+    // Handles the form Submit logic
     const onSubmit = async (event) => {
         event.preventDefault();
-        const { user: { password,email }} = state;
-        let err = {};
 
-        if ( !email || !password) {
-            err.fields = "All fields are required";
-        }
-        if (password.length < 8) {
-            err.password = "Password must be at least 8 characters!";
-        }
-        if (err.length > 0) {
+        const {errors, user: { password,email }} = state;
+    
+
+        if ( !email || !password){
+            errors.message = "All fields are required";
+            setState({...state, errors});
+           return;
+        } 
             
-            setState({...state, errors: err});
+        
+        if (password.length < 8){
+            errors.message = "Password must be at least 8 characters!";
+            setState({...state, errors});
             return;
-        }
-        else {
-            try {
-                
-                const res = await axios.post("http://localhost:5000/signin", state.user );
-                const status = res.status;
-                const result = await res.data;
-                console.log(result, status);
-                if (status === 200 | status === 201) {
-                    setState({...initialState, submitted: true});
-                }
-                else {
-                    setState({...state, errors:result});
-                }
-            } catch (error) {
-                setState({...state, errors:error});
+        } 
+            
+     
+        try {
+            
+            const res = await axios.post("http://localhost:5000/signin", state.user );
+            const status = res.status;
+            const result = await res.data;
+
+            if (status === 200 | status === 201) {
+
+                localStorage.setItem("token",result.token);
+
+                setState({...initialState, submitted: true});
+
             }
+            else {
+                setState({...state, errors:{message : result.toString()}});
+            }
+        } catch (error) {
+            setState({...state, errors:{message : error.toString()}});
         }
 
     };
@@ -68,28 +77,30 @@ export const Login = () => {
     
     return(
     <>
-        <img style={{zIndex: "-1"}} src={wave} alt="wave" className="fixed bottom-0 left-0  h-full"/>
+        <img style={{zIndex: "-1"}} src={wave} alt="wave" className="fixed bottom-0 left-0  h-full wave"/>
         <div className=' m-auto h-screen'>
 
             
             <div className = 'grid contain  h-full pl-8 pr-8'>
-                <div className=' l h-full flex justify-end items-center'>
+                <div className=' l h-full flex justify-end items-center img'>
                     <img width={"500px"} src={phone} alt="signup" className='z-10'/>
                 </div>
 
-                <div className='  h-full flex justify-start items-center text-center'>
+                <div className='  h-full flex justify-start items-center text-center content'>
                     <form style={{width: "360px"}}>
                         <img src={profile} alt="profile" className='h-24 m-auto'/>
                         <h2 className='text-center mt-4 mb-4 ml-0 mr-0 uppercase text-5xl'>Welcome</h2>
-                        {submitted && <p className='text-green-500'>Login Successfully</p>}
-                        <Input error={errors.email}    onChange={handleChange} value={email}   name='email' type='text' label='Email Address' icon = 'at' required/>
+                        {submitted && <p className='text-green-500 mb-2'>Login Successfully</p>}
+                        {errors && <p className='text-red-500 mb-2'>{errors.message}</p>}
+                        <Input     onChange={handleChange} value={email}   name='email' type='text' label='Email Address' icon = 'at' required/>
                     
-                        <Input error={errors.password}  onChange={handleChange} value={password} name='password' type='password' label='Password' icon = 'lock' required/>
+                        <Input   onChange={handleChange} value={password} name='password' type='password' label='Password' icon = 'lock' required/>
 
 
-                        <Link className="block text-right no-underline text-gray-500 duration-300 hover:text-green-300" to="#">Forgot Password?</Link>
-                        
-                        <ButtonAuth  className='text-xl text-white uppercase cursor-pointer  mt-1 block w-full h-12 outline-none border-none bg-green-500' value='Login' type="submit" handleClick={onSubmit} />
+                        <Link className=" float-left text-xs inline-block text-right no-underline text-gray-500 duration-300 hover:text-green-300" to="#">Forgot Password?</Link>
+                        <Link className=" float-right text-xs inline-block text-left no-underline text-gray-500 duration-300 hover:text-green-300" to="/signup">New? Sign Up</Link>
+
+                        <ButtonAuth  className='text-xl text-white uppercase cursor-pointer  mt-1 block w-full h-12 outline-none border-none bg-green-500 mt-3' value='Login' type="submit" handleClick={onSubmit} />
 
                     </form>
                 </div>
--- client/src/pages/auth/signup.jsx
+++ client/src/pages/auth/signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import phone  from "../../assets/undraw_personalization_triu.svg";
 import wave  from "../../assets/wave.png";
 import profile  from "../../assets/undraw_profile_pic_ic5t.svg";
-import "./s.css";
+import "./auth.css";
 import { Input, ButtonAuth } from "../../components/inputs/input";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -34,38 +34,41 @@ export const SignUp = () => {
     const onSubmit = async (event) => {
         event.preventDefault();
         const { user: { username, password, fullname, email, password2 }} = state;
-        let err = {};
+
 
         if (!username || !password || !fullname || !email || !password2) {
-            err.fields = "All fields are required";
-        }
-        if (password.length < 8) {
-            err.password = "Password must be at least 8 characters!";
-        }
-        if (password === password2) {
-            err.password2 = "Both Passwords must be the same";
-        }
-        if (err.length > 0) {
-            
-            setState({...state, errors: err});
+            errors.message = "All fields are required";
+            setState({...state, errors});
             return;
         }
-        else {
-            try {
-                
-                const res = await axios.post("http://localhost:5000/signup", state.user );
-                const status = res.status;
-                const result = await res.data;
-                console.log(result, status);
-                if (status === 200 | status === 201) {
-                    setState({...initialState, submitted: true});
-                }
-                else {
-                    setState({...state, errors:result});
-                }
-            } catch (error) {
-                setState({...state, errors:error});
+
+        if (password.length < 8) {
+            errors.message = "Password must be at least 8 characters!";
+            setState({...state, errors});
+            return;
+        }
+
+        if (password !== password2) {
+            errors.message = "Both Passwords must be the same";
+            setState({...state, errors});
+            return;
+        }
+
+        try {
+            const res = await axios.post("http://localhost:5000/signup", state.user );
+            const status = res.status;
+            const result = await res.data;
+
+            if (status === 200 | status === 201) {
+                localStorage.setItem("token",result.token);
+
+                setState({...initialState, submitted: true});
             }
+            else {
+                setState({...state, errors:{message : result.toString()}});
+            }
+        } catch (error) {
+            setState({...state, errors:{message : error.toString()}});
         }
 
     };
@@ -74,20 +77,22 @@ export const SignUp = () => {
     
     return(
     <>
-        <img style={{zIndex: "-1"}} src={wave} alt="wave" className="fixed bottom-0 left-0  h-full"/>
+        <img style={{zIndex: "-1"}} src={wave} alt="wave" className="fixed bottom-0 left-0  h-full wave"/>
         <div className=' m-auto h-screen'>
 
             
             <div className = 'grid contain  h-full pl-8 pr-8'>
-                <div className=' l h-full flex justify-end items-center'>
-                    <img width={"500px"} src={phone} alt="signup" className='z-10'/>
+                <div className=' h-full flex justify-end items-center img'>
+                    <img width={"500px"} src={phone} alt="signup" className='z-10 '/>
                 </div>
 
-                <div className='  h-full flex justify-start items-center text-center'>
+                <div className=' content  h-full flex justify-start items-center text-center'>
                     <form style={{width: "360px"}}>
                         <img src={profile} alt="profile" className='h-24 m-auto'/>
                         <h2 className='text-center mt-4 mb-4 ml-0 mr-0 uppercase text-5xl'>Welcome</h2>
-                        {submitted && <p className='text-green-500'>Registered Successfully</p>}
+                        {submitted && <p className='text-green-500 mb-2'>Registered Successfully</p>}
+                        {errors && <p className='text-red-500 mb-2'>{errors.message}</p>}
+
                         <Input error={errors.fullname}  onChange={handleChange} value={fullname}   name='fullname' type='text' label='Fullname' icon = 'address-book' required/>
 
                         <Input error={errors.username}  onChange={handleChange} value={username}   name='username' type='text' label='Username' icon = 'user' required/>
@@ -98,7 +103,7 @@ export const SignUp = () => {
 
                         <Input error={errors.password2} onChange={handleChange} value={password2} name='password2' type='password' label='Check Password' icon = 'lock' required/>
 
-                        <Link className="block text-right no-underline text-gray-500 duration-300 hover:text-green-300" to="#">Forgot Password?</Link>
+                        <Link className="block text-right no-underline text-gray-500 duration-300 hover:text-green-300" to="/login">Alread a User? Sign In</Link>
                         
                         <ButtonAuth  className='text-xl text-white uppercase cursor-pointer  mt-1 block w-full h-12 outline-none border-none bg-green-500' value='Sign up' type="submit" handleClick={onSubmit} />
 
